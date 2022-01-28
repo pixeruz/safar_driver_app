2d61eea2b1d6d234601391e372383d2966cdf3c8
muhammadyunusuz <yunuzmuhammad1@gmail.com>
2022-01-28 05:29:48 +0500

    add onboarding ui

--- components/styledComponents.jsx
+++ components/styledComponents.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Text as NativeTextComponent, StyleSheet, View } from "react-native";
+import {
+	Text as NativeTextComponent,
+	StyleSheet,
+	View,
+	Pressable,
+} from "react-native";
 
 export function Text({ ...props }) {
 	return (
@@ -25,6 +30,7 @@ export function Text({ ...props }) {
 					: props.black
 					? "Inter-Black"
 					: "Inter-Regular",
+				color: props.light ? "#fff" : props?.style?.color || "#000",
 			}}
 		/>
 	);
@@ -39,10 +45,32 @@ export function Container({ ...props }) {
 	);
 }
 
+export function Button({ ...props }) {
+	return (
+		<Pressable
+			{...props}
+			style={{
+				...defaultStyles.defaultButtonStyles,
+				...props.style,
+				backgroundColor: props.disabled
+					? "#D3D3D3"
+					: props?.style?.backgroundColor,
+			}}
+		/>
+	);
+}
+
 const defaultStyles = StyleSheet.create({
 	defaultTextStyles: {},
 	defaultContainerStyles: {
 		flex: 1,
-		padding: 10,
+		padding: 16,
+	},
+	defaultButtonStyles: {
+		padding: 16,
+		borderRadius: 6,
+		marginVertical: 5,
+		justifyContent: "center",
+		alignItems: "center",
 	},
 });
--- screens/OnBoardingScreen.jsx
+++ screens/OnBoardingScreen.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Container, Text } from "../components/styledComponents";
+import { Button, Container, Text } from "../components/styledComponents";
 import Logo from "../images/Logo";
 
 export default function Onboardingscreen() {
@@ -10,6 +9,22 @@ export default function Onboardingscreen() {
 			<View style={styles.logoView}>
 				<Logo />
 			</View>
+			<Text bold style={styles.title}>
+				Ketdikmi?
+			</Text>
+			<Text medium style={styles.subtitle}>
+				Safar Driver ilovasini yuklaganingiz uchun rahmat! Ishga tushish
+				uchun ro’yxatdan o’tishingiz kerak!
+			</Text>
+			<View style={styles.space}></View>
+			<Button style={styles.signUpButton}>
+				<Text bold light>
+					Ro’yxatdan o’tish
+				</Text>
+			</Button>
+			<Button style={styles.loginButton}>
+				<Text semiBold>Kirish</Text>
+			</Button>
 		</Container>
 	);
 }
@@ -18,5 +33,27 @@ const styles = StyleSheet.create({
 	logoView: {
 		alignItems: "center",
 		marginTop: 10,
+		marginBottom: 30,
+	},
+	title: {
+		fontSize: 20,
+	},
+	subtitle: {
+		fontSize: 14,
+		lineHeight: 22,
+		marginTop: 5,
+		color: "#73787D",
+	},
+	space: {
+		backgroundColor: "#F7F8F9",
+		flexGrow: 1,
+		marginVertical: 15,
+		borderRadius: 9,
+	},
+	signUpButton: {
+		backgroundColor: "#771E99",
+	},
+	loginButton: {
+		backgroundColor: "#DFE4E9",
 	},
 });
